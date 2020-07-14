204b342fbfbaecd6abec5a191492defae02cd568
Ram Simran <garemella.ramsimran@gmail.com>
2020-07-14 22:49:17 +0530

    First Commit

---
+++ src/main/java/com/example/httpexample/HttpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpExampleApplication.class, args);
+	}
+
+}
